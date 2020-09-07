6a291cb71c4e106704a6c4815c03e71df2c5b144
Rashid <mahraway@gmail.com>
2020-09-07 19:09:24 +0300

    Устранение ошибок

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
 date_default_timezone_set("Europe/Moscow");
 include_once('helpers.php');
 include_once('data.php');
@@ -9,7 +13,7 @@ $title = 'YetiCave'; // Заголовок страницы
 
 
 $main_page = include_template('main.php', ['categories' => $categories, 'lots' => $lots]);
-$main_footer = include_template('footer.php', ['categories' => $categories, 'res' => $res]);
+$main_footer = include_template('footer.php', ['categories' => $categories]);
 $layout_content = include_template('layout.php', [
     'content' => $main_page,
     'footer' => $main_footer,
