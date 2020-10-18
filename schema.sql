3ace755d92bb7c3df359fda231cd9d3db369bb4e
Rashid <mahraway@gmail.com>
2020-10-18 18:30:11 +0300

    Доработки 3.0

--- schema.sql
+++ schema.sql
@@ -1,42 +1,56 @@
+-- Создание базы данных
 CREATE DATABASE yeticave
 	DEFAULT CHARacter SET UTF8
 	DEFAULT COLLATE UTF8_GENERAL_CI;
 
+-- Используем базу данных
 USE yeticave;
 
-
+-- Создание таблицы категорий
 CREATE TABLE categories (
-	id INT AUTO_INCREMENT PRIMARY KEY,
+	category_id INT AUTO_INCREMENT, 
+	name VARCHAR(255) NOT NULL, 
+	simbloic VARCHAR(255) NOT NULL,
+	PRIMARY KEY (category_id) -- указываю первичный ключ
+);
+
+CREATE TABLE users (
+	user_id INT AUTO_INCREMENT,
+	dt_add DATETIME NOT NULL,
 	name VARCHAR(255) NOT NULL,
-	code VARCHAR(255) NOT NULL
+	email VARCHAR(255) NOT NULL UNIQUE, -- поле с проверкой на уникальность
+	pass VARCHAR(255) NOT NULL,
+	contacts VARCHAR(255) NOT NULL,
+	PRIMARY KEY (user_id) -- указываю первичный ключ
 );
 
 CREATE TABLE lots (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	category_id INT REFERENCES categories (id),
+	lot_id INT AUTO_INCREMENT,
+	user_id INT NOT NULL,
+	category_id INT NOT NULL,
 	dt_add DATETIME NOT NULL,
 	name VARCHAR(255) NOT NULL,
 	description VARCHAR(255) NOT NULL,
 	image VARCHAR(255) NOT NULL,
 	price INT NOT NULL,
 	dt_end TIMESTAMP NOT NULL,
-	step INT NOT NULL
+	step INT NOT NULL,
+	PRIMARY KEY (lot_id), 											-- указываю первичный ключ
+	FOREIGN KEY (category_id) REFERENCES categories (category_id),  -- указываю внешний ключ для поля
+	FOREIGN KEY (user_id) REFERENCES users (user_id),				-- указываю внешний ключ для поля
+	INDEX NIndex (name),											-- создаю индекс для поля, по которому будет поиск
+	INDEX CIndex (category_id)										-- создаю индекс для поля, по которому будет поиск
 );
 
 CREATE TABLE bets (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	lot_id INT REFERENCES lots (id),
+	bet_id INT AUTO_INCREMENT,
+	user_id INT NOT NULL,
+	lot_id INT NOT NULL,
 	dt_add DATETIME,
-	price INT NOT NULL
+	price INT NOT NULL,
+	PRIMARY KEY (bet_id), 								-- указываю первичный ключ
+	FOREIGN KEY (user_id) REFERENCES users (user_id),	-- указываю внешний ключ для поля
+	FOREIGN KEY (lot_id) REFERENCES lots (lot_id)		-- указываю внешний ключ для поля
 );
-
-CREATE TABLE users (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	dt_add DATETIME NOT NULL,
-	name VARCHAR(255) NOT NULL,
-	email VARCHAR(255) NOT NULL UNIQUE,
-	password VARCHAR(255) NOT NULL,
-	contacts VARCHAR(255) NOT NULL
-);
-
+ 
 
